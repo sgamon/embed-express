9a751a7cf6adc5fd1d241f659485d9ac66ab487c
Scott Gamon <sgamon@yahoo.com>
2021-03-13 05:21:46 +0000

    moved function list to top

--- index.js
+++ index.js
@@ -5,18 +5,7 @@
 let child_process = require('child_process');
 let fs = require('fs');
 
-[
-  'prompt for package.json file if missing',
-  'prompt for server.js file if it exists',
-  'install dependencies',
-  'create server.js file',
-  'create index.html if needed',
-  'print message',
-].forEach(step => fn[step]())
-
-// -----------------------------------------------------
-
-var fn = {
+let fn = {
   'prompt for package.json file if missing': function () {
     if (!fs.existsSync('./package.json')) {
       console.log(`
@@ -86,4 +75,16 @@ open(\`http://localhost:\${PORT}/\`)
   Express server embedded. Use "node server" to run.
 `)
   },
-}
+};
+
+// -----------------------------------------------------
+
+[
+  'prompt for package.json file if missing',
+  'prompt for server.js file if it exists',
+  'install dependencies',
+  'create server.js file',
+  'create index.html if needed',
+  'print message',
+].forEach(step => fn[step]())
+
