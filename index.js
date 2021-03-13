cd58a126cd065c808693c1f1d1228b6bda7b47de
Scott Gamon <sgamon@yahoo.com>
2021-03-13 04:59:26 +0000

    switch to ISC licence, reformat code, bump major version

--- index.js
+++ index.js
@@ -1,72 +1,89 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
 let child_process = require('child_process');
 let fs = require('fs');
 
-if (!fs.existsSync('./package.json')) {
-  console.log(`
+[
+  'prompt for package.json file if missing',
+  'prompt for server.js file if it exists',
+  'install dependencies',
+  'create server.js file',
+  'create index.html if needed',
+  'print message',
+].forEach(step => fn[step]())
+
+// -----------------------------------------------------
+
+var fn = {
+  'prompt for package.json file if missing': function () {
+    if (!fs.existsSync('./package.json')) {
+      console.log(`
     ERROR: missing package.json
     
     Create package.json using "npm init", then run this command again.
   `);
-  process.exit(1);
-}
+      process.exit(1)
+    }
+  },
 
-if (fs.existsSync('./server.js')) {
-  console.log(`\`
+  'prompt for server.js file if it exists': function () {
+    if (fs.existsSync('./server.js')) {
+      console.log(`\`
     ERROR: server.js already exists
     
     Remove the existing server.js file, then run this command again.
-  `);
-  process.exit(1);
-}
+  `)
+      process.exit(1)
+    }
+  },
 
-child_process.execSync('npm install --save express open');
+  'install dependencies': function () {
+    child_process.execSync('npm install --save express open')
+  },
 
-let serverjs = `
+  'create server.js file': function () {
+    fs.writeFileSync('./server.js', `
 // A very minimal express server.
 
-let express = require('express');
-let open = require('open');
+let express = require('express')
+let open = require('open')
 
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 express()
   .set('port', PORT)
   .use('/', express.static(__dirname))
   .listen(PORT, function() {
-    console.log(\`Server started: http://localhost:\${PORT}/\`);
-  });
+    console.log(\`Server started: http://localhost:\${PORT}/\`)
+  })
 
-open(\`http://localhost:\${PORT}/\`);
-`;
+open(\`http://localhost:\${PORT}/\`)
+`)
+  },
 
-fs.writeFileSync('./server.js', serverjs);
-
-
-if (!fs.existsSync('./index.html')) {
-  let html = `
+  'create index.html if needed': function () {
+    if (!fs.existsSync('./index.html')) {
+      fs.writeFileSync('./index.html', `
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Hello World!</title>
-</head>
-
-<body>
-  <h1>Hello World!</h1>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello World!</title>
+  </head>
+  
+  <body>
+    <h1>Hello World!</h1>
+  </body>
 </html>
+`)
+    }
+  },
 
-`;
-
-  fs.writeFileSync('./index.html', html);
-}
-
-
-console.log(`
+  'print message': function () {
+    console.log(`
   Express server embedded. Use "node server" to run.
-`);
+`)
+  },
+}
